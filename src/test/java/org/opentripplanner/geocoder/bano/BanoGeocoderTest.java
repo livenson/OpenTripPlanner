d60120da57a9d6281cd4fb1b98ff09df101496bf
alajal <anulajal@gmail.com>
2019-12-05 11:36:48 +0200

    Reis-69: test fix

--- src/test/java/org/opentripplanner/geocoder/bano/BanoGeocoderTest.java
+++ src/test/java/org/opentripplanner/geocoder/bano/BanoGeocoderTest.java
@@ -27,9 +27,9 @@ public class BanoGeocoderTest {
 
         boolean found = false;
         for (GeocoderResult result : results.getResults()) {
-            if ("55 Rue du Faubourg Saint-Honoré 75008 Paris".equals(result.getDescription())) {
+            if ("Rue du Faubourg Saint-honoré 75008 Paris".equals(result.getDescription())) {
                 double dist = SphericalDistanceLibrary.distance(result.getLat(),
-                        result.getLng(), 48.870637, 2.316939);
+                        result.getLng(), 48.868637, 2.322939);
                 assert (dist < 100);
                 found = true;
             }
